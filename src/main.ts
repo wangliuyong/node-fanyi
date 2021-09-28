cf612b9d59c44896d3d6814ccf371bd12c0a3370
wly <1355498705@qq.com>
2021-09-28 17:08:37 +0800

    使用百度翻译

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,29 @@
 import * as https from 'https'
 import * as querystring from 'querystring'
-
-
+const md5 = require('md5')
 
 export const translate = (english) => {
   console.log(english);
 
-  const xx = querystring.stringify({
+  const appid = '20180930000214641'
+  const appSecret = 'DbpdusNoDRD4Eu3vieVb'
+  const salt = Math.random()
+  const sign = md5(appid + english + salt + appSecret)
+
+  const query = querystring.stringify({
     q: english,
     from: 'en',
     to: 'zh',
-    appid: '20180930000214641',
-    salt: Math.random(),
-    sign: ''
+    appid,
+    salt,
+    sign
+    // appid+q+salt+密钥的MD5值  
   });
 
   const options = {
-    hostname: 'http://api.fanyi.baidu.com',
+    hostname: 'api.fanyi.baidu.com',
     port: 443,
-    path: '/api/trans/vip/translate',
+    path: '/api/trans/vip/translate?' + query,
     method: 'GET'
   };
 
