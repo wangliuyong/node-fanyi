a9cebb264f5a55ca23db66a6aee1d3eb627d0eac
wly <1355498705@qq.com>
2021-09-28 16:54:49 +0800

    引入https &&  md5

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,14 @@
 import { Command } from 'commander';
 const pkg = require("../package.json");
 
+import { translate } from './main'
+
 const program = new Command();
-program.version(pkg.version).name('fanyi');
+program.version(pkg.version).name('fanyi')
+  .usage('<English>')
+  .argument('<English>')
+  .action((english) => {
+    translate(english)
+  });
 
 program.parse(process.argv);
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,35 @@
-console.log('hi');
+import * as https from 'https'
+import * as querystring from 'querystring'
+
+
+
+export const translate = (english) => {
+  console.log(english);
+
+  const xx = querystring.stringify({
+    q: english,
+    from: 'en',
+    to: 'zh',
+    appid: '20180930000214641',
+    salt: Math.random(),
+    sign: ''
+  });
+
+  const options = {
+    hostname: 'http://api.fanyi.baidu.com',
+    port: 443,
+    path: '/api/trans/vip/translate',
+    method: 'GET'
+  };
+
+  const req = https.request(options, (res) => {
+    res.on('data', (d) => {
+      process.stdout.write(d);
+    });
+  });
+  req.on('error', (e) => {
+    console.error(e);
+  });
+  req.end();
+
+}
